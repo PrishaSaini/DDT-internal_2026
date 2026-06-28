8d635a6064af0da9b89eb00ace285761f5da41ee
PrishaSaini <prisha.saini@gmail.com>
2026-06-28 23:37:52 +1200

    Add files via upload

---
+++ DDTSecond-handstationaryinternal/screen/dashboard.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+class DashboardScreen:
+    def __init__(self, root, user_name="User"):
+     self.root= root
+    
+     self.frame=tk.Frame(root)
+     self.frame.pack(pady=50)
+    
+    tk.Label(
+        self.frame, 
+        text =f"Wecome, {user_name}!",
+        font=("Arial",14)
+       ).pack(pady=10)
+    
+    tk.Label(
+        self.frame, 
+        text ="DashBoard",
+        font=("Arial",20, "bold")
+       ).pack(pady=10)
+    
+    tk.Label(
+        self.frame, 
+        text ="You have succesfully logged in",
+       ).pack(pady=10)
+    
+    
