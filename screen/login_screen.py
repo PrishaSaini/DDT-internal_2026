b55c4e2cbbe38ffd2bd9acd2459aa377c7afa149
PrishaSaini <prisha.saini@gmail.com>
2026-07-02 22:55:52 +1200

    This is to correct the duplicate folder problem. Copying from the old file and some changes to Loginscreen (as test).

---
+++ screen/login_screen.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+class LoginScreen:
+    def __init__(self, root):
+        self.root = root
+
+        self.frame = tk.Frame(root) 
+        self.frame.pack(pady=50)
+
+        tk.Label(self.frame, text="Login", font=("Arial", 16)).pack(pady=10)
+        
+        tk.Label(self.frame, text="Email").pack() 
+        self.email_entry = tk.Entry(self.frame)
+        self.email_entry.pack()
+        
+        tk.Label(self.frame, text="Password").pack() 
+        self.password_entry = tk.Entry(self.frame, show="*")
+        self.password_entry.pack()
+
+        tk.Button(self.frame, text="Login", command=self.login).pack(pady=10)
+        tk.Button(self.frame, text="Go to Sign Up", command=self.go_to_signup).pack(pady=5)
